956a3e5979e9e07eff9ef74a1b7261bad2e8695e
NahueMendez <nahueldanielmendez@gmail.com>
2026-03-24 21:23:36 -0300

    Docs updated to v1.2.0

--- mftoolkit/__init__.py
+++ mftoolkit/__init__.py
@@ -3,7 +3,7 @@ from .crossovers import SPIC, CDVA
 from .mfsources import iaaft_surrogate, shuffle_surrogate
 from .mfgeneration import generate_fgn, generate_mf_corr, generate_mf_dist, generate_crossover_series
 
-__version__ = "1.0.0"
+__version__ = "1.2.0"
 
 __all__ = [
     'mfdfa',
